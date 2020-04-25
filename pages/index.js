06aac329ec93bc71165745a649a0d10fa2ffa8aa
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-25 12:35:14 +0300

    next-store initial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div className='Home'>
+      <Head>
+        <title>Home</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Home;
