ca740c4b9d3d1a994f50417f2a5b1af27a8a6f8a
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-25 12:37:53 +0300

    laptops SSG initial

---
+++ pages/laptop/[id].js
@@ -0,0 +1,45 @@
+import fetch from 'node-fetch';
+// import { useRouter } from 'next/router'
+
+export default function Laptop({laptop}) {
+  return (
+    <div className='laptop'>
+      <h1>{laptop.name}</h1>
+      <ul>
+        <li>{laptop.id}</li>
+        <li><img src={laptop.image} /></li>
+      </ul>
+    </div>
+  )
+  
+}
+
+export async function getStaticPaths() {
+  const laptops = await fetch('http://localhost:3004/laptops').then(res => res.json());
+  const response = laptops.map((laptop) => {
+    return { id: laptop.id };
+  })
+
+  const paths = response.map((laptop) => ({
+    params: { id: laptop.id }
+  }))
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({params}) {
+  const res = await fetch(`http://localhost:3004/laptops/${params.id}`).then(res => res.json());
+  const laptop = {
+    id: res.id,
+    name: res.name,
+    // cpu: laptop.att_cpu_details,
+    image: res.image
+  }
+
+  
+ 
+
+  return {
+    props: { laptop }
+  };
+}
