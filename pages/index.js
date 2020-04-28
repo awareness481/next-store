8fca94c3d4640cd974aa7edec59f0056dc64d6d0
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-28 09:40:15 +0300

    added basic pagination and item render

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,87 @@
-import Head from 'next/head';
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+import fetch from "node-fetch";
+import LaptopThumbnail from "../components/LaptopThumbnail";
+import { List } from 'antd';
 
-const Home = () => {
+const Home = ({ laptops }) => {
+  // const router = useRouter();
+  const [data, setData] = useState("none");
+  const [laptopBrand, setLaptopBrand] = useState("Lenovo");
+
+  useEffect(() => {
+    const filteredLaptops = laptops.filter((laptop) => {
+      return laptop.brand === laptopBrand;
+    });
+
+    setData(filteredLaptops);
+  }, [laptopBrand]);
+
   return (
-    <div className='Home'>
+    <div className="Home">
       <Head>
         <title>Home</title>
       </Head>
+      <div className="select-brand">
+        <label htmlFor="laptop-brand-select">Select a brand:</label>
+        <select
+          name="brand"
+          id="laptop-brand-select"
+          className="select-brand__dropdown"
+          value={laptopBrand}
+          onChange={(e) => setLaptopBrand(e.target.value)}
+        >
+          <option value="Lenovo">Lenovo</option>
+          <option value="Dell">Dell</option>
+          <option value="HP">HP</option>
+        </select>
+      </div>
+      <div className="results">
+        <List 
+          grid={{ gutter: 8, column: 5 }}
+          size='large'
+          pagination={{
+            pageSize: 10,
+            total: data.length,
+            data: data,
+            size: 'small'
+          }}
+          dataSource={data}
+          renderItem={item => (
+            <List.Item
+              key={item.name}
+              extra={
+                <a href={`/laptop/${item.id}`} className="thumbnail" id={item.id} >
+                  <img src={item.image} />
+                </a>
+              }
+            />)
+          }
+        />
+      </div>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3004/laptops").then((res) =>
+    res.json()
+  );
+  const laptops = response.map((laptop) => {
+    return {
+      id: laptop.id,
+      name: laptop.name,
+      brand: laptop.att_brand,
+      image: laptop.image,
+    };
+  });
+
+  return {
+    props: {
+      laptops,
+    },
+  };
 }
 
 export default Home;
