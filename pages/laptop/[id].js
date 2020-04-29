fa08def38bb6b95502c55f5ae6fbaf4ce6639169
Jesse Jafa <jesse.jafa@gmail.com>
2020-04-29 09:24:39 +0300

    laptop product page initial structure and styling

--- pages/laptop/[id].js
+++ pages/laptop/[id].js
@@ -1,14 +1,34 @@
 import fetch from 'node-fetch';
-// import { useRouter } from 'next/router'
+import { Descriptions } from 'antd';
 
 export default function Laptop({laptop}) {
+  const Item = Descriptions.Item;
+
   return (
     <div className='laptop'>
-      <h1>{laptop.name}</h1>
-      <ul>
-        <li>{laptop.id}</li>
-        <li><img src={laptop.image} /></li>
-      </ul>
+      <img src={laptop.image} className='laptop--image' />
+      <Descriptions 
+        title="Specs"
+        bordered={true}
+        layout="horizontal"
+        column={2}
+      >
+        <Item label='Name'>{laptop.name}</Item>
+        <Item label='Brand'>{laptop.brand}</Item>
+        <Item label='CPU'>{laptop.cpu}</Item>
+        <Item label='GPU'>{laptop.gpu}</Item>
+        <Item label='Operating system'>{laptop.os}</Item>
+        <Item label='Resolution'>{laptop.resolution}</Item>
+        <Item label='Available RAM'>{laptop.ram}</Item>
+        <Item label='Screen size'>{laptop.screen}</Item>
+        <Item label='SSD'>
+          {
+            laptop.ssd ? laptop.ssd : 'Not available'
+          }
+        </Item>
+        <Item label='Total storage'>{laptop.storage}</Item>
+        <Item label='Weight'>{laptop.weight}</Item>
+      </Descriptions>
     </div>
   )
   
@@ -33,8 +53,18 @@ export async function getStaticProps({params}) {
   const laptop = {
     id: res.id,
     name: res.name,
-    // cpu: laptop.att_cpu_details,
-    image: res.image
+    brand: res.att_brand,
+    cpu: res.att_cpu_details,
+    image: res.image,
+    price: res.price,
+    gpu: res.att_gpu,
+    os: res.att_os,
+    resolution: res.att_pixels_x + "x" + res.att_pixels_y,
+    ram: res.att_ram + "GB",
+    screen: res.att_screen_size + '"',
+    ssd: 0 ? undefined : res.att_ssd + "GB",
+    storage: res.att_storage + "GB",
+    weight: res.att_weight + " kg"
   }
 
   
